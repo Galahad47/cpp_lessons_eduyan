efbe3da30860554ae23a6e2814fbc9f7f20e2757
Galahad47 <ak19gangsta@gmail.com>
2025-02-20 20:59:54 +0300

    Добавьте файлы проекта.

---
+++ check/check.cpp
@@ -0,0 +1,20 @@
+﻿//Ферзь задачка
+#include <iostream>
+int x,y,z,e;
+using namespace std;
+int main() {
+    cout << "Enter point x1,x2,y1,y2"; cin >> x >> y >>z >>e;
+    if (x == y) {
+        if (z == e) {
+            if (abs(x - y)) {
+                if (abs(z - e)) {
+                    cout << "yes";
+                }
+            }
+        }
+    }
+    else
+    {
+        cout << "No";
+    }
+}
