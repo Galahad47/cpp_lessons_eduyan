5030447d24f9e59ee293ef605b2f999f8dd0ba0d
Galahad47 <ak19gangsta@gmail.com>
2025-02-20 20:59:54 +0300

    Добавил новые файлы

---
+++ pryam/pryam.cpp
@@ -0,0 +1,17 @@
+﻿//Задачака пирамида
+#include <iostream>
+using namespace std;
+int x, y, z;
+int main()
+{
+	cout << "Enter num: "; cin >> x >> y >>z;
+    if (x + y <= z || x + z <= y || y + z <= x) {
+        std::cout << "UNDEFINED\n";
+    }
+    else if (x * x + y * y == z * z || x * x + z * z == y * y || y * y + z * z == x * x) {
+        std::cout << "YES\n";
+    }
+    else {
+        std::cout << "NO\n";
+    }
+}
