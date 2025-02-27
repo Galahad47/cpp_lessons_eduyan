e43aacbda71e4167a92c112947282d05386f6bd4
Galahad47 <ak19gangsta@gmail.com>
2025-02-27 19:39:00 +0300

    Осбо ничего нового

---
+++ calendar/calendar.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int dayweek, daysmounth,i;
+	cout << "Введите день недели от 1 - 7 и кол-во дней в месяце: "; cin >> dayweek >> daysmonth;
+	for (i = 1; i < dayweek; ++i {
+		cout << "  ";
+	}
+	for (int day = 1; day < = daysmounth; ++day) {
+		if (day < 10) {
+			cout << " ";
+		}
+		cout << day;
+			if (dayweek == 7) {
+				cout << "\n";
+				dayweek = 1;
+
+			}
+			else
+			{
+				cout << " ";
+				dayweek += 1;
+			}
+		}
+	if (dayweek != 1) {
+		cout << "\n";
+	}
+}
--- check/check.cpp
+++ check/check.cpp
@@ -3,18 +3,19 @@
 int x,y,z,e;
 using namespace std;
 int main() {
-    cout << "Enter point x1,x2,y1,y2"; cin >> x >> y >>z >>e;
+    cout << "Enter point x1,x2,y1,y2"; 
+    cin >> x >> y >>z >>e;
     if (x == y) {
         if (z == e) {
             if (abs(x - y)) {
                 if (abs(z - e)) {
-                    cout << "yes";
+                    cout << "YES";
                 }
             }
         }
     }
     else
     {
-        cout << "No";
+        cout << "NO";
     }
 }
